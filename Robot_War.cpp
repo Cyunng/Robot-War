ccc548448964e376f998588443dad8badf449b3d
Cyunng <chiyuanng2005@gmail.com>
2025-05-24 15:53:56 +0800

    Initial files

---
+++ Robot_War.cpp
@@ -0,0 +1,34 @@
+/**********|**********|**********|
+Program: Robot_War.cpp / Robot.h
+Course: Object-Oriented Programming and Data Structures
+Trimester: 2420
+Name1: Ng Chi Yuan
+ID: 242UC244F5
+Email: NG.CHI.YUAN@student.mmu.edu.my
+Phone: 017-7855626
+Name2: Jireh Chai E-Lok
+ID: 242UC244M5
+Email: JIREH.CHAI.ELOK@student.mmu.edu.my
+Phone: 012-5131505
+Name3: Lee Ru Zhen
+ID: 243UC247CN
+Email: LEE.RU.ZHEN@student.mmu.edu.my
+Phone: 011-53530330
+Name4: 
+ID: 
+Email: HERVIN.KUMAR.BALOO@student.mmu.edu.my
+Phone: 018-9713530
+Lecture Section: TC3L
+Tutorial Section: T12L
+**********|**********|**********/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    
+    return 0;
+}
+
