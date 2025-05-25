62fdc996f8d2360a9b19f16f085e768410d2084c
Hervink <hervinkumar33@gmail.com>
2025-05-25 22:45:01 +0800

    Update BattleField.h

--- BattleField.h
+++ BattleField.h
@@ -2,7 +2,42 @@
 #define BATTLEFIELD_H
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+void displayBattlefield(int cols, int rows) {
+    cout << "    ";
+    for (int c = 0; c < cols; ++c) {
+        cout << "  " << setw(2) << setfill('0') << c << "  ";
+    }
+    cout << endl;
+
+    cout << "    +";
+    for (int c = 0; c < cols; ++c) {
+        cout << "-----+";
+    }
+    cout << endl;
+
+    for (int r = 0; r < rows; ++r) {
+        cout << setw(2) << setfill('0') << r << "  |";
+        for (int c = 0; c < cols; ++c) {
+            cout << "     |";
+        }
+        cout << endl;
+
+        cout << "    |";
+        for (int c = 0; c < cols; ++c) {
+            cout << "     |";
+        }
+        cout << endl;
+
+        cout << "    +";
+        for (int c = 0; c < cols; ++c) {
+            cout << "-----+";
+        }
+        cout << endl;
+    }
+}
 
 
 
