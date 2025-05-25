386bd159b64821f3bd47e3c90e7187db15fccb64
Hervink <hervinkumar33@gmail.com>
2025-05-25 22:36:37 +0800

    Update Robot class & manage structure of Robot War

--- BattleField.h
+++ BattleField.h
@@ -2,8 +2,41 @@
 #define BATTLEFIELD_H
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
+inline void displayBattlefield(int cols, int rows) {
+    cout << "    ";
+    for (int c = 0; c < cols; ++c) {
+        cout << "  " << setw(2) << setfill('0') << c << "  ";
+    }
+    cout << endl;
 
+    cout << "    +";
+    for (int c = 0; c < cols; ++c) {
+        cout << "-----+";
+    }
+    cout << endl;
+
+    for (int r = 0; r < rows; ++r) {
+        cout << setw(2) << setfill('0') << r << "  |";
+        for (int c = 0; c < cols; ++c) {
+            cout << "     |";
+        }
+        cout << endl;
+
+        cout << "    |";
+        for (int c = 0; c < cols; ++c) {
+            cout << "     |";
+        }
+        cout << endl;
+
+        cout << "    +";
+        for (int c = 0; c < cols; ++c) {
+            cout << "-----+";
+        }
+        cout << endl;
+    }
+}
 
 #endif
