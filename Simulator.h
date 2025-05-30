afdbe87bf16265c713b7fabf2669733246e3874b
Cyunng <chiyuanng2005@gmail.com>
2025-05-30 19:25:09 +0800

    delete uncessarities and modify errors

--- Simulator.h
+++ Simulator.h
@@ -7,7 +7,7 @@
 #include <queue>
 #include <fstream>
 #include <string>
-#include <cstdlib> // include this to use rand()
+#include <cstdlib>
 
 using namespace std;
 
@@ -16,30 +16,35 @@ private:
     Battlefield battlefield_; // For the game world
     string inputFile_; // To input file
     string outputfile_; // To output log file 
+    bool initialized_ = false; // Track initialization status
 
 public:
     Simulator() = default; // This is to construct a new simulator object
 
     ~Simulator() = default; // This is to destroy the simulator object
 
-    void initalize(const string& inputFile) {
+    bool initalize(const string& inputFile) {
         inputFile_ = inputFile;
+        initialized_ = false;
 
-        srand(1211109038); // To seed random number generator with a fixed seed (We use leader's ID)
+        srand(1211109335); // To seed random number generator with a fixed seed (We use leader's ID)
 
+        // Read and parse input file
         if (!battlefield_.readFile(inputFile_)) {
             cout << "Failed to read the input file" << endl;
-            return;
+            return false;
         }
 
-        if (!battlefield_.placeRobots()) {
-            cout << "Failed to place robots on the battlefield." << endl;
-            return;
-        }
+        initialized_ = true;
+        return true;
     }
 
     // To run the complete simulation
     void run() {
+        if (!initialized_) {
+            cout << "Error: Simulation is not nitialized" << endl;
+        }
+
         cout << "===== Battlefield Started  =====" << endl;
         battlefield_.displayBattlefield();
 
@@ -55,30 +60,39 @@ public:
         outputfile_ = filename;
     }
 
+    bool isInitialized() const {
+        return initialized_;
+    }
+
 private:
     // To display the final results in output file
-    void displayResults() cosnt {
-        ofstream* out = &cout;
+    void displayResults() const {
         ofstream fileStream;
+        bool useFile = false;
 
         if (!outputfile_.empty()) {
             fileStream.open(outputfile_);
             if (fileStream.is_open()) {
-                out = &fileStream;
-            } else {
+                useFile = true;
+            }
+            else {
                 cout << "Could not open output file!" << outputfile_ << endl;
             }
         }
+
+        ostream& out = useFile ? fileStream : cout;
     
-        *out << "\n===== Battlefield Results =====" << endl;
-        *out << "Total turns played: " << battlefield_.getCurrentTurn() << endl;
+        out << "\n===== Battlefield Results =====" << endl;
+        out << "Total turns played: " << battlefield_.getCurrentTurn() << endl;
 
         if (battlefield_.getRobotCount() == 1) {
-            *out << "Winner: Last remaining robot" << endl;
-        } else if (battlefield_.getRobotCount() == 0) {
-            *out << "No winner: All robots destroyed" << endl;
-        } else {
-            *out << "Game ended with " << battlefield_.getRobotCount() << " robots still alive" << endl;
+            out << "Winner: Last remaining robot" << endl;
+        }
+        else if (battlefield_.getRobotCount() == 0) {
+            out << "No winner: All robots destroyed" << endl;
+        }
+        else {
+            out << "Game ended with " << battlefield_.getRobotCount() << " robots still alive" << endl;
         }
         
         if (fileStream.is_open()){
